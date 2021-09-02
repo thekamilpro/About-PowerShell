1e182427c73c62920f8f57a57ca851370f6f8863
Kamil Procyszyn <kprocyszyn@gmail.com>
2021-09-02 20:36:48 +0100

    looping, conditionals

--- PowerShell Hashtable/PowerShell Hashtable.ps1
+++ PowerShell Hashtable/PowerShell Hashtable.ps1
@@ -8,6 +8,11 @@ $hash = @{}
 $hash.Add("Person", "John")
 $hash.Add("Email", "John@john.com")
 $hash.Add("Pet", "Cat")
+$hash.Add("Surname", "Smith")
+$hash
+
+# Removing Key from hashtable
+$hash.Remove("Surname")
 $hash
 
 # Prepopulating hashtable
@@ -55,3 +60,69 @@ $details
 $details.pet = "Dog"
 $details.Food = "Lamb Shish Kebab"
 $details
+
+###########
+# Looping #
+###########
+
+# using foreach
+foreach ($key in $details.Keys) {
+    "{0} is {1}" -f $key, $details[$key]
+}
+
+# piping to foreach-object
+$details.Keys | ForEach-Object {
+    "{0} is {1}" -f $_, $details[$_]
+}
+
+# GetEnumerator() allows us to work with properties
+$details.GetEnumerator() | ForEach-Object {
+    "{0} is {1}" -f $_.Key, $_.Value
+}
+
+# Values can't be updated while enumarated!
+foreach ($key in $details.Keys) {
+    $details[$key] = 'BLAH'
+}
+
+# Keys must be cloned before updating
+$details.Keys.Clone() | ForEach-Object {
+    $details[$_] = "SANITISED"
+}
+$details
+
+######
+# IF #
+######
+
+# checking if hashtable exists
+if ($details) {"It's there"}
+
+$empty = @{}
+if ($empty) {"It's empty, but it's there!"}
+
+# Checking if key exists
+if ($details.Person) {"There's some info about a person"}
+
+# Deailing with false
+$details.Add("Empty", "")
+$details.Add("Null", $Null)
+$details.Add("False", $false)
+$details
+
+if ($details.Empty) {"It's there"}
+if ($details.Null) {"It's there"}
+if ($details.False) {"It's there"}
+
+if ($details.ContainsKey('Null')) {"It's null!"}
+
+######################
+# Custom expressions #
+######################
+
+$employee = @{
+    Name = "Beth"
+    HourlyWage = 10
+    "Hours worked" = 7.5
+    }
+$employee | Select-Object -Property  *,@{N = "Wage"; E = {$_.HourlyWage * $_."Hours Worked" } }
