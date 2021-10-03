9bdab0787aa50355914c2e3a6c1d8e7a698f89ae
Kamil Procyszyn <kprocyszyn@gmail.com>
2021-10-03 17:02:47 +0100

    hashtables and properties

--- PowerShell PsCustomObject/PowerShell PsCustomObject.ps1
+++ PowerShell PsCustomObject/PowerShell PsCustomObject.ps1
@@ -23,3 +23,58 @@ $obj = [PSCustomObject]@{
     Type = "Tabby cat"
 }
 $obj
+$obj.Name
+
+
+
+# Saving to a file (JSON):
+# We could save CSV, however CSV doesn't support nested properties, thus why JSON is preferable 
+$Path = "$env:TEMP\pstojson.json"
+$obj | ConvertTo-Json -Depth 99 | Set-Content -Path $path
+$imported = Get-Content -Path $Path
+$imported
+$imported | ConvertFrom-Json
+
+##############
+# PROPERTIES #
+##############
+
+# Accessing properties
+$obj.Species
+
+# Dynamically accessing properties
+$prop = 'Type'
+$obj.$prop
+
+# Adding property
+$obj | Add-Member -MemberType NoteProperty -Name 'Favourite Snack' -Value 'Dental treats'
+$obj.'Favourite Snack'
+
+# Removing properties
+$obj.psobject.Properties.Remove('Favourite Snack')
+$obj.'Favourite Snack'
+
+# Listing properteis
+$obj.psobject.properties.name
+$obj | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+
+#############
+# Hashtable #
+#############
+
+# Converting hashtable to psobject:
+$ht = @{
+    Name = "Mr Fiber"
+    Species = "Domestic cat"
+    Type = "Tabby cat"
+}
+$htobj = [pscustomobject]$ht
+$htobj
+
+# Converting psobject to hashtable
+$newHt = @{}
+foreach ($property in $htobj.psobject.Properties.name) {
+    $newHt[$property] = $htobj.$property
+}
+$newHt
+
