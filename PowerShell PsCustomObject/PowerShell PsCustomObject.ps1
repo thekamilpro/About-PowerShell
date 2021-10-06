c79b2225667270c07d09327cec0956ad8643ba78
Kamil Procyszyn <kprocyszyn@gmail.com>
2021-10-06 21:04:47 +0100

    types and methods

--- PowerShell PsCustomObject/PowerShell PsCustomObject.ps1
+++ PowerShell PsCustomObject/PowerShell PsCustomObject.ps1
@@ -18,9 +18,9 @@ About PsCustomObject:
 
 # Creating custom object:
 $obj = [PSCustomObject]@{
-    Name = "Mr Fiber"
+    Name    = "Mr Fiber"
     Species = "Domestic cat"
-    Type = "Tabby cat"
+    Type    = "Tabby cat"
 }
 $obj
 $obj.Name
@@ -64,9 +64,9 @@ $obj | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
 
 # Converting hashtable to psobject:
 $ht = @{
-    Name = "Mr Fiber"
+    Name    = "Mr Fiber"
     Species = "Domestic cat"
-    Type = "Tabby cat"
+    Type    = "Tabby cat"
 }
 $htobj = [pscustomobject]$ht
 $htobj
@@ -78,3 +78,82 @@ foreach ($property in $htobj.psobject.Properties.name) {
 }
 $newHt
 
+###########
+# Methods #
+###########
+
+# We can actually have some action!
+$method = {
+    "Hi, my name is $($this.name) and I like to sleep."
+}
+
+$params = @{
+    MemberType = "ScriptMethod"
+    Name       = "SayHi"
+    Value      = $method
+}
+$obj | Add-Member @params
+
+# How about, converting Object to Hashtable, as the actual method?
+$params = @{
+    MemberType = "ScriptMethod"
+    Name       = "OutHashtable"
+    Value      = {
+        $hash = @{}
+        $this.psobject.properties.name.foreach({
+                $hash[$_] = $this.$_
+            })
+        return $hash
+    }
+}
+$obj | Add-Member @params
+
+#########
+# Types #
+#########
+
+$obj | GM # now it is PSCustomObject, duh
+
+$obj.psobject.TypeNames # this looks familiar
+$obj.psobject.TypeNames.Insert(0, "Kp.CatsAreAwesome")
+$obj | GM
+
+function Fun {
+    param ( 
+        [PSTypeName('Kp.CatsAreAwesome')]
+        [Parameter(ValueFromPipeline)]
+        $Cat 
+        )
+        
+        if ($PSBoundParameters.ContainsKey('Cat')) {
+            "Here's an awesome cat:"
+            return $Cat.SayHi()
+        } else {
+            "Cats are awesome!"
+        }
+}
+
+# We can also specify type name at creation
+$obj2 = [pscustomobject]@{
+    PSTypeName = 'Kp.CatsAreAwesome'
+    Name = 'Whiskers'
+    Species = "Domestic cat"
+    Type    = "Persian"
+}
+
+$method = {
+    "Hi, my name is $($this.name) and I like to sleep."
+}
+
+$params = @{
+    MemberType = "ScriptMethod"
+    Name       = "SayHi"
+    Value      = $method
+}
+$obj2 | Add-Member @params
+$obj2 | fun
+
+# Specify default output
+Update-TypeData -TypeName kp.CatsAreAwesome -DefaultDisplayPropertySet Name,Type
+$obj # list is now limited to only Name and Type properties
+$obj | fl * #displays all
